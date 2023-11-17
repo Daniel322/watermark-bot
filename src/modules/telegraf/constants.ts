4e14fc0237d2d28b74d0b57a13bd255ca9b12060
Erik Sulimenko <e.sulimenko@dunice.net>
2023-11-17 13:49:02 +0300

    add on photo listener, add recieving file buf

---
+++ src/modules/telegraf/constants.ts
@@ -0,0 +1,10 @@
+export const MESSAGES = {
+  WELCOME: 'Добро пожаловать!\nОтправьте изображение для нанесения водного знака',
+  BAD_REQUEST: 'Что-то пошло не так. Попробуй еще раз',
+  ASK_TEXT: 'Оправьте текст',
+}
+
+export const SYS_MESSAGES = {
+  NO_FILE_IN_MESSAGE: 'NO_FILE_IN_MESSAGE',
+  FILE_REQUEST_ERROR: 'FILE_REQUEST_ERROR',
+}
--- src/modules/telegraf/telegraf.service.ts
+++ src/modules/telegraf/telegraf.service.ts
@@ -1,14 +1,22 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Telegraf, type Context } from 'telegraf';
+import { message } from 'telegraf/filters';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 import { Bind } from '@common/decorators';
+
+import { MESSAGES, SYS_MESSAGES } from './constants';
 
 @Injectable()
 export class TelegrafService implements OnModuleInit, OnModuleDestroy {
   private bot: Telegraf;
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
 
   onModuleInit(): void {
     const token = this.configService.get<string>('telegram.token');
@@ -26,10 +34,39 @@ export class TelegrafService implements OnModuleInit, OnModuleDestroy {
 
   setListeners(): void {
     this.bot.start(this.onStart);
+    this.bot.on(message('photo'), this.onPhoto);
   }
 
   @Bind
   onStart(ctx: Context): void {
-    ctx.reply('Welcome');
+    ctx.reply(MESSAGES.WELCOME);
+  }
+
+  @Bind
+  async onPhoto(ctx: Context): Promise<void> {
+    try {
+      if ('photo' in ctx.message) {
+        const [file] = ctx.message.photo;
+        if (file == null) throw new Error(SYS_MESSAGES.NO_FILE_IN_MESSAGE);
+
+        const fileLink = await this.bot.telegram.getFileLink(file.file_id);
+        const buf = await this.getFile(fileLink.href);
+        // TODO add saving buffer into cache with user id as key
+        await ctx.reply(MESSAGES.ASK_TEXT);
+      }
+    } catch(error) {
+      console.error(error.message);
+      await ctx.reply(MESSAGES.BAD_REQUEST);
+    }
+  }
+
+  async getFile(url: string): Promise<ArrayBuffer> {
+    try {
+      const request = this.httpService.get<ArrayBuffer>(url, { responseType: 'arraybuffer' });
+      const { data } = await firstValueFrom(request);
+      return data;
+    } catch(error) {
+      throw new Error(SYS_MESSAGES.FILE_REQUEST_ERROR);
+    }
   }
 }
