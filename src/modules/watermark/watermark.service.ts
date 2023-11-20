28935182fd0db6b69d6d8909f3b365d8c4f650e4
Daniil Kravchenko <kravchenkodanil122@gmail.com>
2023-11-20 16:32:58 +0300

    start to work on pattern type logic

--- src/modules/watermark/watermark.service.ts
+++ src/modules/watermark/watermark.service.ts
@@ -16,7 +16,16 @@ export class WatermarkService {
     text,
     options,
   }: SetWatermarkOnPhotoForTelegrafType): Promise<Buffer> {
-    const watermarkIconBuffer = this.generateWatermarkSvg({ text, ...options });
+    const { width, height }: sharp.Metadata = await sharp(file).metadata();
+
+    console.log(width, height, options);
+
+    const watermarkIconBuffer = this.generateWatermarkSvg({
+      text,
+      ...options,
+      imageHeight: height,
+      imageWidth: width,
+    });
 
     const imageWithWatermark = sharp(file)
       .composite([
@@ -34,46 +43,58 @@ export class WatermarkService {
   generateWatermarkSvg({
     text,
     size = 's',
+    type = 'single',
+    imageWidth,
+    imageHeight,
   }: GenerateWatermarkSvgProps): Buffer {
-    const { width, height, fontSize } = this.generateSizes(size);
-    const svg = `
-      <svg width="${width}" height="${height}">
-      <style>
-      .title { fill: #fff; font-size: ${fontSize}px; font-weight: bold; textAlign: left }
-      </style>
-      <text x="1%" y="30%" text-anchor="start" class="title">${text}</text>
-      </svg>
-    `;
+    console.log(imageWidth, imageHeight);
+    if (type === 'single') {
+      const { width, height, fontSize } = this.generateSizes(size, text.length);
 
-    return Buffer.from(svg);
+      if (width > imageWidth) {
+        throw new Error('WATERMARK_SIZE_MORE_THAN_ORIGINAL_IMAGE');
+      }
+      const svg = `
+        <svg width="${width}" height="${height}">
+        <style>
+        .title { fill: #fff; font-size: ${fontSize}px; font-weight: bold; textAlign: left }
+        </style>
+        <text x="1%" y="30%" text-anchor="start" class="title">${text}</text>
+        </svg>
+      `;
+
+      return Buffer.from(svg);
+    } else {
+      const square: number = imageWidth * imageHeight;
+    }
   }
 
-  generateSizes(size: Size = 's') {
+  generateSizes(size: Size = 's', textLength: number) {
     switch (size) {
       case 's': {
         return {
-          width: 180,
+          width: 10 * textLength,
           height: 50,
           fontSize: 20,
         };
       }
       case 'm': {
         return {
-          width: 350,
+          width: 20 * textLength,
           height: 100,
           fontSize: 40,
         };
       }
       case 'l': {
         return {
-          width: 600,
+          width: 30 * textLength,
           height: 150,
           fontSize: 60,
         };
       }
       default: {
         return {
-          width: 150,
+          width: 20 * textLength,
           height: 50,
           fontSize: 20,
         };
